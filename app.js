ced0e187ba8fcb75fa1106e0d86b9c34c364a045
Gonzalo Bahamondez <gonzalo@becual.com>
2016-11-26 13:11:32 -0300

    testing hook

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const app = express();
 
-console.log('hello');
-console.log('hello');
-console.log('hello');
 
 app.get('/', async (req, res) => {
     res.send({
